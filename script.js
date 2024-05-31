72b536cfeb5794cbeb390e5aa01dc0e041db9343
samonvercel <samonvercel7777@gmail.com>
2024-05-31 10:27:00 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,37 @@
+var menuBtn = document.querySelector(".menu-btn");
+var closeBtn = document.querySelector(".close-btn");
+var headerContain = document.querySelector(".header-contain2");
+
+menuBtn.addEventListener("click", () => {
+  headerContain.style.transition = "0.3s";
+  setTimeout(() => {
+    headerContain.style.left = "0%";
+  }, 1);
+});
+
+closeBtn.addEventListener("click", () => {
+  headerContain.style.transition = "0.3s";
+  setTimeout(() => {
+    headerContain.style.left = "-100%";
+  }, 1);
+});
+
+var swiper = new Swiper(".cubeSwiper", {
+  // effect: "cube",
+  slidesPerView: 1,
+  spaceBetween: 20,
+  loop: true,
+  autoplay: true,
+  interval: 6000,
+  grabCursor: true,
+  pauseOnMouseEnter: true,
+});
+
+var closeMessage = document.querySelector(".close-ms");
+var message = document.querySelector(".message");
+
+closeMessage.addEventListener("click", () => {
+  message.style.display = "none";
+
+  console.log("Close");
+});
